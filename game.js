35c38dead2efaf37245f140fefb3078411a1fd53
13ilaurente <13ilaurente@stmacademy.org.uk>
2019-06-12 14:05:22 +0100

    1 file modified

--- game.js
+++ game.js
@@ -1,14 +1,12 @@
 import MenuScene from './scenes/MenuScene';
 
-let menuScene = new MenuScene();
-
 let config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  scene: [ MenuScene ]
 };
 
 let game = new Phaser.Game(config);
-game.scene.add('MenuScene', menuScene);
-game.scene.start('MenuScene');
+
 
