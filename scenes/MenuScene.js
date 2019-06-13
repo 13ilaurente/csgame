c44d6ad24f87f7b9e44a094e79dff6fd602619bd
13ilaurente <13ilaurente@stmacademy.org.uk>
2019-06-13 09:26:52 +0100

    2 files modified

--- scenes/MenuScene.js
+++ scenes/MenuScene.js
@@ -3,10 +3,11 @@ class MenuScene extends Phaser.Scene {
     super({key: 'MenuScene'});
   }
   preload() {
-    this.load.image('background_image', './assests/forest-background.png');
+    this.load.image('background', './assests/backgrounds/forest-background.png');
   }
   create() {
     this.add.text(20, 20, "Loading game...");
+    this.background = this.add.image(0, 0, "background")
   }
 }
 
