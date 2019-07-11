2acd0c11e3633074b9ae8655d4c8d0c9270c8f6a
13ilaurente <13ilaurente@stmacademy.org.uk>
2019-07-11 21:17:36 +0100

    1 file modified

--- scenes/MenuScene.js
+++ scenes/MenuScene.js
@@ -1,6 +1,6 @@
 export default class MenuScene extends Phaser.Scene {
   constructor() {
-    super({key: 'MenuScene'});
+    super({ key: 'MenuScene' });
   }
   preload() {
     this.load.image('background', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assests/backgrounds/forest-background.png'
@@ -8,8 +8,26 @@ export default class MenuScene extends Phaser.Scene {
   }
   create() {
     this.background = this.add.image(0, 0, "background");
-    this.background.setOrigin(0,0);
+    this.background.setOrigin(0, 0);
     this.background.setScale(3);
+
+    this.gameButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
+    this.centerButton(this.gameButton, 1);
+
+    this.gameText = this.add.text(0, 0, 'Play', { fontSize: '32px', fill: '#fff' });
+    this.centerButtonText(this.gameText, this.gameButton);
+
+    this.gameButton.on('pointerdown', function (pointer) {
+      this.scene.start('Game');
+    }.bind(this));
+
+    this.input.on('pointerover', function (event, gameObjects) {
+      gameObjects[0].setTexture('blueButton2');
+    });
+
+    this.input.on('pointerout', function (event, gameObjects) {
+      gameObjects[0].setTexture('blueButton1');
+    });
   }
 }
 
