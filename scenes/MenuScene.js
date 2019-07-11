e09712a8472eda808de218ea936613d33d971bc4
13ilaurente <13ilaurente@stmacademy.org.uk>
2019-07-11 21:50:24 +0100

    1 file modified, 1 added, 1 deleted

--- scenes/MenuScene.js
+++ scenes/MenuScene.js
@@ -5,11 +5,14 @@ export default class MenuScene extends Phaser.Scene {
   preload() {
     this.load.image('background', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assests/backgrounds/forest-background.png'
     );
+    this.load.image('playButton', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assests/play.png');
   }
   create() {
     this.background = this.add.image(0, 0, "background");
     this.background.setOrigin(0, 0);
     this.background.setScale(3);
+
+    this.playButton = this.add.sprite(100, 200, 'playButton').setInteractive();
   }
 }
 
