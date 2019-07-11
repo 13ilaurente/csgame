c6fe26907d84d500a8991f2e2072d49011c160e0
13ilaurente <13ilaurente@stmacademy.org.uk>
2019-07-11 21:18:46 +0100

    1 file modified

--- scenes/MenuScene.js
+++ scenes/MenuScene.js
@@ -10,24 +10,6 @@ export default class MenuScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, "background");
     this.background.setOrigin(0, 0);
     this.background.setScale(3);
-
-    this.gameButton = this.add.sprite(100, 200, 'blueButton1').setInteractive();
-    this.centerButton(this.gameButton, 1);
-
-    this.gameText = this.add.text(0, 0, 'Play', { fontSize: '32px', fill: '#fff' });
-    this.centerButtonText(this.gameText, this.gameButton);
-
-    this.gameButton.on('pointerdown', function (pointer) {
-      this.scene.start('Game');
-    }.bind(this));
-
-    this.input.on('pointerover', function (event, gameObjects) {
-      gameObjects[0].setTexture('blueButton2');
-    });
-
-    this.input.on('pointerout', function (event, gameObjects) {
-      gameObjects[0].setTexture('blueButton1');
-    });
   }
 }
 
