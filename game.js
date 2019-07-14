205771925ec5286fb25ac29d6f71a20657983f26
13ilaurente <13ilaurente@stmacademy.org.uk>
2019-07-14 22:31:31 +0100

    2 files modified

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
 import menuScene from './scenes/menuScene';
+import Game from './scenes/Game';
 
 const config = {
   type: Phaser.AUTO,
-  width: 272, 
-  height: 160, 
-  scene: [ menuScene ]
+  width: 272,
+  height: 160,
+  scene: [menuScene, Game]
 };
 
 let game = new Phaser.Game(config);
--- scenes/Game.js
+++ scenes/Game.js
@@ -4,9 +4,13 @@ export default class Game extends Phaser.Scene {
   }
 
   preload() {
- 
+    this.load.spritesheet('player', 'https://github.com/13ilaurente/csgame/blob/master/assests/sprites/Player/adventurer/adventurer-v1.5-Sheet.png', {framewidth: 55, frameheight: 37});
   }
 
   create() {
+    this.background = this.add.image(0, 0, "background");
+    this.background.setOrigin(0, 0);
 
+    const player = this.add.sprite(100, 100, 'player', 0);
   }
+}
