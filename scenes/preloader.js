a4af237fb0658da2f2d57d2dc11dfe06da19ef5d
13ilaurente <13ilaurente@stmacademy.org.uk>
2019-07-15 23:45:31 +0100

    1 file modified, 6 added, 6 deleted

--- scenes/preloader.js
+++ scenes/preloader.js
@@ -4,14 +4,14 @@ export default class preloaderScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('forest-background', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assests/backgrounds/forest-background.png'
+    this.load.image('forest-background', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assets/backgrounds/forest-background.png'
     );
-    this.load.image('mountain-background', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assests/backgrounds/mountain-background.png')
+    this.load.image('mountain-background', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assets/backgrounds/mountain-background.png')
 
-    this.load.image('playButton', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assests/buttons/play.png');
+    this.load.image('playButton', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assets/buttons/play.png');
 
-    this.load.image('playerFall0', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assests/sprites/Player/adventurer/Individual%20Sprites/adventurer-fall-00.png');
-    this.load.image('playerFall1', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assests/sprites/Player/adventurer/Individual%20Sprites/adventurer-fall-01.png');
+    this.load.image('playerFall0', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assets/sprites/Player/adventurer/Individual%20Sprites/adventurer-fall-00.png');
+    this.load.image('playerFall1', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assets/sprites/Player/adventurer/Individual%20Sprites/adventurer-fall-01.png');
   }
 
   update() {
