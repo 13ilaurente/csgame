a720ba47938f6cdc5630f2ba7756ff94e4a6c6cc
13ilaurente <13ilaurente@stmacademy.org.uk>
2019-07-17 12:18:16 +0100

    1 file modified

--- scenes/preloader.js
+++ scenes/preloader.js
@@ -14,11 +14,13 @@ export default class preloaderScene extends Phaser.Scene {
 
     this.load.image('playerFall0', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assets/sprites/player/individual%20sprites/adventurer-fall-00.png');
     this.load.image('playerFall1', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assets/sprites/player/individual%20sprites/adventurer-fall-01.png');
-
-    this.load.spritesheet('player', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assets/sprites/player/adventurer.png', {frameWidth: 50, frameHeight: 37});
   }
 
   update() {
     this.scene.start('menuScene');
   }
 }
+
+/*
+this.load.spritesheet('player', 'https://cdn.jsdelivr.net/gh/13ilaurente/csgame@master/assets/sprites/player/adventurer.png', {frameWidth: 50, frameHeight: 37});
+*/
