222b2fa204d4d5fdadefafb641c919213c1f5482
13ilaurente <13ilaurente@stmacademy.org.uk>
2019-12-02 11:11:22 +0000

    3 files added, 1 deleted

--- classes/player.js
+++ classes/player.js
@@ -1,4 +1,4 @@
-export default class character extends Phaser.GameObjects.Sprite {
+export default class sprite extends Phaser.GameObjects.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number, hp: number, texture: string, frame: string | number) {
     super(scene, x, y, hp, mana, texture, frame)
   }
