cceb74f395610affa988d35269662b33f732fe6a
13ilaurente <13ilaurente@stmacademy.org.uk>
2019-12-16 10:36:11 +0000

    3 files modified

--- classes/sprite.js
+++ classes/sprite.js
@@ -1,6 +1,13 @@
 export default class sprite extends Phaser.GameObjects.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y);
+    this.setTexture("playerFall");
+    this.setPosition(x, y);
+  }
+  /*
   constructor(config) {
     super(config.scene, config.x, config.y, config.hp, config.mana, config.texture, config.frame, config.key)
     config.scene.add.existing(this);
   }
+  */
 }
--- scenes/game.js
+++ scenes/game.js
@@ -9,6 +9,7 @@ export default class gameScene extends Phaser.GameObjects.Sprite {
     this.background = this.add.image(0, -50, "forest-background");
     this.background.setOrigin(0, 0);
 
-    let player = new Player({scene: this, x: 100, y: 100, hp: 100, mana: 100, texture: 'player', frame: 23});
+    this.add.existing(new player(this, 100, 100));
+    //let player = new Player({scene: this, x: 100, y: 100, hp: 100, mana: 100, texture: 'player', frame: 23});
   }
 }
